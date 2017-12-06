e2d7ae68161e37f1ab45520d3e51fcecc377c47d
ErmakMichael <31983903+ErmakMichael@users.noreply.github.com>
2017-12-06 18:41:33 +0300

    no message

--- src/com/intexsoft/task18/Task18.java
+++ src/com/intexsoft/task18/Task18.java
@@ -5,17 +5,17 @@ public class Task18 {
 	public static void main(String[] args) {
 		String s = "Hello, im., !!!!::::Mi,ke!";
 		int sumSymbol = 0;
-		sumSymbol += Task18.Search(',', s, 0);
-		sumSymbol += Task18.Search('.', s, 0);
-		sumSymbol += Task18.Search('!', s, 0);
-		sumSymbol += Task18.Search(':', s, 0);
+		sumSymbol += Task18.SearchAndSum(',', s, 0);
+		sumSymbol += Task18.SearchAndSum('.', s, 0);
+		sumSymbol += Task18.SearchAndSum('!', s, 0);
+		sumSymbol += Task18.SearchAndSum(':', s, 0);
 
 		String text = String.format("We have %s symbol(s)!", sumSymbol);
 		System.out.println(text);
 
 	}
 
-	private static int Search(char c, String s, int index) {
+	public static int SearchAndSum(char c, String s, int index) {
 		int n = 0;
 		while (index != -1) {
 			index = s.indexOf(c, index);

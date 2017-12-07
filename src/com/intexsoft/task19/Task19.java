c90c366865263927a0c1b71ca7fa04a801639d6d
ErmakMichael <31983903+ErmakMichael@users.noreply.github.com>
2017-12-07 21:48:29 +0300

    +20

--- src/com/intexsoft/task19/Task19.java
+++ src/com/intexsoft/task19/Task19.java
@@ -3,18 +3,18 @@ package com.intexsoft.task19;
 public class Task19 {
 
 	public static void main(String[] args) {
-		String s = "sdfm, , sn23 rnn 23nfkls vlcv sd fdv";
+		String s = "sdfm,   312   sn23 rnn 23nfkls vlcv sd fdv";
 		s = s.trim();
-		
+
 		String[] splitResult = s.split("\\s+|,\\s*");
-		
+
 		int n = splitResult.length;
-		
+
 		for (String i : splitResult) {
 			System.out.print(i + ", ");
 		}
-		
-		System.out.println("\n\nWe have word(s): "+n);
+
+		System.out.println("\nWe have word(s): " + n);
 	}
 
 }

0ee865bd369c33d799f44b3389bd36a03dd9be56
ErmakMichael <31983903+ErmakMichael@users.noreply.github.com>
2017-12-25 16:01:48 +0300

    +28

---
+++ src/com/itacademy/task28/Task28.java
@@ -0,0 +1,36 @@
+package com.itacademy.task28;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.Random;
+
+public class Task28 {
+
+	public static void main(String[] args) {
+
+		ArrayList<Integer> assessments = new ArrayList<Integer>();
+		Random random = new Random();
+
+		int size = random.nextInt(10);
+
+		System.out.println("Size array: " + size);
+
+		for (int i = 0; i < size; i++) {
+			assessments.add(random.nextInt(10) + 1);
+			System.out.print(assessments.get(i) + " ");
+		}
+
+		System.out.println();
+
+		Iterator<Integer> iterator = assessments.iterator();
+		int maxNumber = iterator.next();
+		while (iterator.hasNext()) {
+			int temp = iterator.next();
+			if (maxNumber < temp) {
+				maxNumber = temp;
+			}
+		}
+		System.out.println("\nMax number: " + maxNumber);
+	}
+
+}
